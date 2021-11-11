0dbd589183de30b0e6dadca5a21b5bde5eef8647
Cláudio de Oliveira Pereira <claudio.pereira@inpa.gov.br>
2021-11-11 17:36:26 -0400

    feat: add buttons and actions to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,23 @@
+import { useHistory } from "react-router";
+
 const Home = (): JSX.Element => {
-  return <h1>Home</h1>;
+  const history = useHistory();
+
+  const handleRedirectToLogin = (): void => {
+    history.push("/login");
+  };
+
+  const handleRedirectToSignUp = (): void => {
+    history.push("/signup");
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      <button onClick={handleRedirectToLogin}>Entre</button>
+      <button onClick={handleRedirectToSignUp}>Cadastre-se</button>
+    </>
+  );
 };
 
 export default Home;

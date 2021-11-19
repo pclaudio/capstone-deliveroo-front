d033ab4b50a239a697d3b7ea6e9dc473c59c0118
Jerlysson Miranda <jerlyssonm@gmail.com>
2021-11-19 00:10:13 -0300

    fix: loop correct

--- src/components/PerfilUser/index.tsx
+++ src/components/PerfilUser/index.tsx
@@ -1,62 +1,59 @@
 import { useAuthentication } from "../../providers/Authentication";
 import { usePerfil } from "../../providers/PerfilProvider";
 import Button from "../Button";
-import edit from "../../assets/img/Edit.svg"
+import edit from "../../assets/img/Edit.svg";
 import Menu from "../Menu";
 import {
-    Imagem,
-    Container,
-    ContainerMaster,
-    H1,
-    Edit,
-    Voucher,
-    Favorito
-  } from "./styles";
+  Imagem,
+  Container,
+  ContainerMaster,
+  H1,
+  Edit,
+  Voucher,
+  Favorito,
+} from "./styles";
 
 const PerfilUser = () => {
-const { handleLogout } = useAuthentication();
+  const { handleLogout } = useAuthentication();
 
-const handleLogoutClick = (): void => {
+  const handleLogoutClick = (): void => {
     handleLogout();
-};
-const {
-    photo,
-    getPhoto,
-    profile,
-    getProfile
-} = usePerfil();
+  };
+  const { photo, getPhoto, profile, getProfile } = usePerfil();
 
-const handlePerfil = () => {
+  const handlePerfil = () => {
     getProfile();
     getPhoto();
-}; 
-handlePerfil();
-return (
+  };
+  // handlePerfil();
+  return (
     <ContainerMaster>
-      {photo.map((item) => (
-        <Imagem src={item.link} alt=""/>
+      {photo.map((item, indice) => (
+        <Imagem src={item.link} key={indice} />
       ))}
       <Container>
-      {profile.map((item) => {
-        return (
-          <div key={item.id}>
-            <H1>
-              <b>
-                {item.firstName} {item.lastName}
-              </b>
-              <Edit src={edit} alt="" />
-            </H1>
-          </div>
-        );
-      })}
-      <Voucher>component do voucher aqui </Voucher>
+        {profile.map((item) => {
+          return (
+            <div key={item.id}>
+              <H1>
+                <b>
+                  {item.firstName} {item.lastName}
+                </b>
+                <Edit src={edit} alt="" />
+              </H1>
+            </div>
+          );
+        })}
+        <Voucher>component do voucher aqui </Voucher>
 
-      <H1 className="fav"> Favorite </H1>
+        <H1 className="fav"> Favorite </H1>
         <Favorito> imagem do favorito aqui mockado </Favorito>
-        <Button className="logout" onClick={handleLogoutClick}>Logout</Button>
+        <Button className="logout" onClick={handleLogoutClick}>
+          Logout
+        </Button>
       </Container>
       <Menu />
     </ContainerMaster>
   );
-}
+};
 export default PerfilUser;
